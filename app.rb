23dac0af80844c53c4857998230dddcd05794307
Milton <miltonsoftwareengineer@gmail.com>
2022-10-15 17:18:39 +0300

    completed assignment

---
+++ app.rb
@@ -0,0 +1,22 @@
+#console.log in js
+#parenthesis is allowed but the most
+#prefered is the one below
+puts "Hello World!" 
+
+#outputing strings with puts and print
+#prints and puts does the same thing but puts
+#adds a line break at end of string
+
+# print "Hello world!"
+# print "Hello world!"
+# print "Hello world!"
+print "Pass this test, please.\n"
+#Inspecting data with p and pp
+# puts [1, 2, 3]
+
+p [1, 2, 3]
+
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+
+#You can also use the --fail-fast, or --f-f flag to tell RSpec to stop running after the first failing test
